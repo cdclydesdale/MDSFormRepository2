179e94e9201d885048918b32ec5ff72f99fef7aa
cdclydesdale <30913733+cdclydesdale@users.noreply.github.com>
2017-08-15 15:04:32 -0400

    Imported Platform Dependency
    import org.openqa.selenium.Platform;

--- src/test/java/com/amind/NewTest.java
+++ src/test/java/com/amind/NewTest.java
@@ -8,6 +8,7 @@ import java.awt.event.KeyEvent;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
